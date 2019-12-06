d89daf3b1a8fe1b9d8d42dfbdf3e7387df0fdea9
yashverma <yash.verma1200@gmail.com>
2019-12-07 00:40:45 +0530

    Initial commit

---
+++ app/src/main/java/com/cipher0007/sidenavigation/MainActivity.java
@@ -0,0 +1,65 @@
+package com.cipher0007.sidenavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    NavController navController;
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    AppBarConfiguration appBarConfiguration;
+  //  BottomNavigationView bottomNavigationView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        init();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+       // NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    private void init() {
+        drawer = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+       // bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        navController = Navigation.findNavController(this,R.id.main);
+        appBarConfiguration = new AppBarConfiguration.Builder(new int[]{R.id.homeFragment,R.id.termsCondition})
+                .setDrawerLayout(drawer)
+                .build();
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController,appBarConfiguration );
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
